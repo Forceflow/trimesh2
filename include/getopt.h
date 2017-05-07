4b9e9ed5adae9b6b9e4be5026f9036cc6aa0d16e
Jeroen Baert <jerbae@gmail.com>
2017-05-07 14:52:16 +0200

    Cleaned up gist for getopt

--- include/getopt.h
+++ include/getopt.h
@@ -1,11 +1,8 @@
-// Put this in a separate .h file (called "getopt.h").
-// The prototype for the header file is:
-/*
-#ifndef GETOPT_H
-#define GETOPT_H
-int getopt(int nargc, char * const nargv[], const char *ostr) ;
-#endif
-*/
+#pragma once
+
+// This header contains a getopt alternative for MSVC compilers
+// Original gist from: https://gist.github.com/superwills/5815344
+
 /*
 * Copyright (c) 1987, 1993, 1994
 *      The Regents of the University of California.  All rights reserved.
@@ -42,37 +39,33 @@ int getopt(int nargc, char * const nargv[], const char *ostr) ;
 #include <string.h>
 #include <stdio.h>
 
-int     opterr = 1,             /* if error message should be printed */
-optind = 1,             /* index into parent argv vector */
-optopt,                 /* character checked for validity */
-optreset;               /* reset getopt */
-char    *optarg;                /* argument associated with option */
+int     opterr = 1, //if error message should be printed 
+optind = 1, //index into parent argv vector
+optopt, //character checked for validity
+optreset; // reset getopt */
+char    *optarg; //* argument associated with option
 
 #define BADCH   (int)'?'
 #define BADARG  (int)':'
 #define EMSG    ""
 
-								/*
-								* getopt --
-								*      Parse argc/argv argument vector.
-								*/
 int getopt(int nargc, char * const nargv[], const char *ostr)
 {
-	static char *place = EMSG;              /* option letter processing */
-	const char *oli;                              /* option letter list index */
+	static char *place = EMSG; // option letter processing
+	const char *oli; // option letter index
 
-	if (optreset || !*place) {              /* update scanning pointer */
+	if (optreset || !*place) { // update scanning pointer
 		optreset = 0;
 		if (optind >= nargc || *(place = nargv[optind]) != '-') {
 			place = EMSG;
 			return (-1);
 		}
-		if (place[1] && *++place == '-') {      /* found "--" */
+		if (place[1] && *++place == '-') { // found
 			++optind;
 			place = EMSG;
 			return (-1);
 		}
-	}                                       /* option letter okay? */
+	} /// option letter okay?
 	if ((optopt = (int)*place++) == (int)':' ||
 		!(oli = strchr(ostr, optopt))) {
 		/*
@@ -87,15 +80,15 @@ int getopt(int nargc, char * const nargv[], const char *ostr)
 			(void)printf("illegal option -- %c\n", optopt);
 		return (BADCH);
 	}
-	if (*++oli != ':') {                    /* don't need argument */
+	if (*++oli != ':') { // don't need an argument
 		optarg = NULL;
 		if (!*place)
 			++optind;
 	}
-	else {                                  /* need an argument */
-		if (*place)                     /* no white space */
+	else { // need an argument
+		if (*place) // no white space
 			optarg = place;
-		else if (nargc <= ++optind) {   /* no arg */
+		else if (nargc <= ++optind) { //no arg
 			place = EMSG;
 			if (*ostr == ':')
 				return (BADARG);
@@ -103,10 +96,10 @@ int getopt(int nargc, char * const nargv[], const char *ostr)
 				(void)printf("option requires an argument -- %c\n", optopt);
 			return (BADCH);
 		}
-		else                            /* white space */
+		else // white space
 			optarg = nargv[optind];
 		place = EMSG;
 		++optind;
 	}
-	return (optopt);                        /* dump back option letter */
+	return (optopt); //dump back option letter
 }
